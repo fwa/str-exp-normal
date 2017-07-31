15555bf9296d1bb8e7fee9c862b136481f55caab
fwa <fwa@users.noreply.github.com>
2017-07-31 10:50:05 -0400

    create association.R outline

---
+++ association.R
@@ -0,0 +1,32 @@
+# make SL table
+# make expression table
+
+library(data.table)
+library(VennDiagram)
+library(pbapply)
+
+load("kw.RData")
+# patients with both exp and str: exp, repeats
+
+### get p and statistics: case 1,2,3
+#r square
+
+# ss vs ll
+
+# ss vs sl vs ll
+
+### case done: association
+
+# adjust p
+
+# add trend
+
+# count num for sig 4 level
+
+# intersect with Gymrek and count
+
+# venn plot
+
+# draw picture for top 3
+
+# wetlab check
